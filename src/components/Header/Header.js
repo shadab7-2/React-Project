aaea47da9bd0459f703a5814fb48b875eb4ea903
Sahdab Ansari <sahdabansari972@gmail.com>
2023-07-04 23:35:58 +0530

    adding functionality in navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,34 @@ import React, { useState } from "react";
 import "./Header.css";
 import { BiMenuAltRight } from "react-icons/bi";
 import OutsideClickHandler from "react-outside-click-handler";
+import Residencies from "../Residencies/Residencies";
+import Value from "../Value/Value";
+import Contact from "../Contact/Contact";
+import GetStarted from "../GetStarted/GetStarted"
 
 const Header = () => {
+  const [activeSection, setActiveSection] = useState(null);
   const [menuOpened, setMenuOpened] = useState(false);
+
   const getMenuStyles = (menuOpened) => {
     if (document.documentElement.clientWidth <= 800) {
       return { right: !menuOpened && "-100%" };
+    }
+  };
+  const HandleSectionClick = (section) => {
+    setActiveSection(section);
+    setMenuOpened(false);
+  };
+  const renderSection = () => {
+    switch (activeSection) {
+      case "residencies":
+        return <Residencies />;
+      case "ourvalue":
+        return <Value/>;
+      case "contactus":
+        return <Contact />;
+      case "getstarted":
+        return <GetStarted/>;
     }
   };
   return (
@@ -21,10 +43,38 @@ const Header = () => {
           }}
         >
           <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
-            <a href="#">Residencies</a>
-            <a href="#">Our Value</a>
-            <a href="#">Contact Us</a>
-            <a href="#">Get Started</a>
+            <a
+              href="#"
+              onClick={() => {
+                HandleSectionClick("residencies");
+              }}
+            >
+              Residencies
+            </a>
+            <a
+              href="#"
+              onClick={() => {
+                HandleSectionClick("ourvalue");
+              }}
+            >
+              Our Value
+            </a>
+            <a
+              href="#"
+              onClick={() => {
+                HandleSectionClick("contactus");
+              }}
+            >
+              Contact Us
+            </a>
+            <a
+              href="#"
+              onClick={() => {
+                HandleSectionClick("getstarted");
+              }}
+            >
+              Get Started
+            </a>
             <a className="button" href="mailto:sandabansari972@gmail.com">
               Contact
             </a>
@@ -38,6 +88,7 @@ const Header = () => {
           <BiMenuAltRight size={30} />
         </div>
       </div>
+      {renderSection()}
     </section>
   );
 };
